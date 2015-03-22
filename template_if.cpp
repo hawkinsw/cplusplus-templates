0c0cfc0b5067d8ada83b63ed901bbb70c380c2c7
Will Hawkins <hawkinsw@opentechinstitute.org>
2015-03-21 23:39:20 -0400

    Split template_calculation according to actual contents.

---
+++ template_if.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+template <bool b>
+struct _if
+{
+template <typename T, typename F, const T *t, const F *f>
+struct result 
+{
+	/*
+	 * Constexpr required here and below 
+	 * because the pointers (though they are
+	 * actually constant) are not integral types.
+	 * 'static const' initialization is only
+	 * allowed with integral types. constexpr
+	 * changed that in c++11. Must compile with
+	 * -std=c++11 for this to work.
+	 */
+	constexpr static const T *value = t;
+};
+};
+
+template <>
+struct _if<false>
+{
+template <typename T, typename F, const T *t, const F *f>
+struct result 
+{
+	constexpr static const F *value = f;
+};
+};
+
+/*
+ * For _if to work, these must have "static"
+ * external linkage. Or maybe internal linkage,
+ * but I don't know how to do that.
+ */
+int one;
+string false_;
+string true_;
+int main() {
+
+	one = 1;
+	false_ = "false";
+	true_ = "true";
+
+	cout << "_if<false, \"true\", \"false\">: "
+	     << *(_if<false>::result<string, string, &true_, &false_>::value) << endl;
+	cout << "_if<true, \"true\", \"false\">: "
+	     << *(_if<true>::result<string, string, &true_, &false_>::value) << endl;
+	cout << "_if<true, 1, \"false\">: "
+	     << *(_if<true>::result<int, string, &one, &false_>::value) << endl;
+	return 0;
+}
